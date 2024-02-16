2a6628594b2e339f8682d21551a06a98e0b00e4a
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-02-16 19:14:39 -0300

    Criando componentes e estilizando na pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import { Container, Form, SubmitButton } from './styles';
+
 
 export default function Main() {
     return(
-        <Title>
-            Página Main
-        </Title>
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositorios
+            </h1>
+
+            <Form onSubmit={() => {}}>
+                <input type="text" placeholder="Adicionar Repositorios" />
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
         
     );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,46 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-   color:#FF0000;
-   
+export const Container = styled.div`
+      max-width: 700px;
+      background: #FFF;
+      border-radius: 4px;
+      box-shadow: 0 0 20px rgba(0,0,0, 0.2);
+      padding: 30px;
+      margin: 80px auto;
+
+      h1 {
+        font-size: 20px;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+
+        svg {
+            margin-right: 10px
+        }
+      }
+`;
+
+export const Form = styled.form`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+
+    input {
+        flex: 1;
+        border: 1px solid #DDD;
+        padding: 10px 15px;
+        border-radius: 4px;
+        font-size: 17px;
+    }
+`;
+
+export const SubmitButton = styled.button`
+    background: #0D2636;
+    border: 0;
+    border-radius: 4px;
+    margin-left: 10px;
+    padding: 0 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
