72a23781c547143eba0dfd83dfe583f926561046
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-02-19 17:41:23 -0300

    Carregando os detalhes na página repositorio

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container } from './styles';
+import api from '../../services/api';
+
+
 
 export default function Repositorio({match}) {
+
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function load() {
+          const nomeRepo = decodeURIComponent(match.params.repositorio);
+          const [repositorioData, issuesData] = await Promise.all([
+            api.get(`/repos/${nomeRepo}`),
+            api.get(`/repos/${nomeRepo}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5
+                }
+            })
+          ]);
+          setRepositorio(repositorioData.data);
+          setIssues(issuesData.data);
+          setLoading(false);
+        }
+        load();
+    }, [match.params.repositorio]);
+
     return(
-        <h1 style={{color:'#FFF'}}>
-            {decodeURIComponent(match.params.repositorio)}
-        </h1>
+        <Container>
+            
+        </Container>
     );
 }
