f2b3b0af8180f6e4c060bf1b532b2d242370c62a
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-02-19 16:33:54 -0300

    passando o nome do repositorio por parametro para a página repositorio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa'
 import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 
@@ -103,9 +104,9 @@ export default function Main() {
                             </DeleteButton>
                             {repo.name}
                         </span>
-                        <a href="">
+                        <Link to={`/repositorio/${encodeURIComponent(repo.name)}`}>
                             <FaBars size={20}/>
-                        </a>
+                        </Link>
                     </li>
                 ))}        
             </List>
--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default function Repositorio() {
+export default function Repositorio({match}) {
     return(
-        <h1>Página Repositorio</h1>
+        <h1 style={{color:'#FFF'}}>
+            {decodeURIComponent(match.params.repositorio)}
+        </h1>
     );
 }
