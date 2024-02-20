05ae5f42727a62932053d32c8670d14ffcf7de5d
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-02-19 21:12:25 -0300

    listando as issues na página de detalhes e estilizando.

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Owner, Loading, BackButton } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
@@ -51,6 +51,23 @@ export default function Repositorio({match}) {
                 <h1>{repositorio.name}</h1>
                 <p>{repositorio.description}</p>
             </Owner>
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
         </Container>
     );
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -50,3 +50,63 @@ export const BackButton = styled(Link)`
     outline: 0;
     background: transparent;
 `;
+
+export const IssuesList = styled.ul`
+    margin-top: 30px;
+    padding-top: 30px;
+    border-top: 1px solid #eee;
+
+    li {
+        list-style: none;
+        display: flex;
+        padding: 15px 10px;
+
+        & + li {
+           margin-top: 12px;
+        }
+    }
+
+    img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        border: 2px solid #0D2636;
+    }
+
+    div {
+        flex: 1;
+        margin-left: 12px;
+
+        p {
+            margin-top: 10px;
+            font-size: 12px;
+            color: #000;
+        }
+    }
+    
+    strong {
+        font-size: 15px;
+
+        a { 
+            text-decoration: none;
+            color: #222;
+            transition: 0.3s;
+
+            &:hover {
+                color: #0071db;
+            }
+        }
+
+        span {
+            background: #222;
+            color: #FFF;
+            border-radius: 4px;
+            font-size: 12px;
+            font-weight: 600;
+            padding: 5px 7px;
+            margin-left: 10px;
+
+        }
+
+    }    
+`;
